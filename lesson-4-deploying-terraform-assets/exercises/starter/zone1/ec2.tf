1afc8d1fa30db8661d6a1fe57975ef5c9ccda6da
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-11-03 09:07:43 +0000

    complete ec2 instance details

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -1,10 +1,11 @@
   module "project_ec2" {
-   source             = 
-   instance_count     = 
-   name               = local.name
+   source             = "./modules/ec2"
+   instance_count     = 2
+   instance_type      = t3.micro
+   name               = Ubuntu.Web
    account            = data.aws_caller_identity.current.account_id
    aws_ami            = "ami-0b9064170e32bde34"
    private_subnet_ids = module.vpc.private_subnet_ids
-   public_subnet_ids  = 
-   vpc_id             = 
+   public_subnet_ids  = module.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_id
  }
