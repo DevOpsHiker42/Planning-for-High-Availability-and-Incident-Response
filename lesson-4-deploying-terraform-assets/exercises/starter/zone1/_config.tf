880d40c936bff0b129619ee337e0d96ff7e6de58
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-11-03 09:37:19 +0000

    update bucket name

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-jaturner"
+     bucket = "udacity-tf-jaturner2"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
