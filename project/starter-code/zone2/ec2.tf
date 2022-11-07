70bb5f7094ece194f742a7356f37c9094ba98f6b
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-11-07 18:32:47 +0000

    increase Zone2 EC2 web server count to 3

--- project/starter-code/zone2/ec2.tf
+++ project/starter-code/zone2/ec2.tf
@@ -9,11 +9,11 @@
 
   module "project_ec2" {
    source             = "./modules/ec2"
-   instance_count     = var.instance_count
+   instance_count     = 3
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
    aws_ami            = "ami-06d4307efaabbeb72"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
-   public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+   public_subnet_ids  = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
  }
